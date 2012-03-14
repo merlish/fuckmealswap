018ff4179c51693147fdd01b631cb7c8e9b78a6c
unknown <merlyn@merlyn-virt-PC.(none)>
2012-03-14 22:31:40 +0000

    a good commit

--- FuckMealSwap/Global.asax.cs
+++ FuckMealSwap/Global.asax.cs
@@ -45,7 +45,7 @@ namespace FuckMealSwap
 
             // Use LocalDB for Entity Framework by default
             Database.DefaultConnectionFactory = new SqlConnectionFactory("Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
-
+           
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
 
---
+++ FuckMealSwap/Models/MealQueueModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Linq;
+
+namespace FuckMealSwap.Models
+{
+    public class MealQueueContext : DbContext
+    {
+        public MealQueue sundayQueue { get; set; }
+        public MealQueue todayQueue { get; set; }
+    }
+
+    public class MealQueue
+    {
+        public DateTime MealWaitStart { get; set; }
+        public DateTime MealWaitEnd { get; set; }
+        public Dictionary<string, DateTime> UserLastCheckedIn { get; set; }
+
+        public List<string> ValidUsers
+        {
+            get
+            {
+                var threshold = DateTime.Now.AddSeconds(-60);
+                return new List<string>(UserLastCheckedIn.Keys.Where<string>(name => UserLastCheckedIn[name] >= threshold));
+            }
+        }
+
+        public MealQueue(DateTime start, DateTime end)
+        {
+            MealWaitStart = start; MealWaitEnd = end; 
+            UserLastCheckedIn = new Dictionary<string, DateTime>();
+        }
+    }
+}
